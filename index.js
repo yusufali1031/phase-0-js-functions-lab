bdd5435eb81a3661bbf6eb91606efeb3f59ae4e9
yusuf <yusuf.ali1031@gmail.com>
2026-03-22 21:25:56 +0200

    Completed all functions for JS Functions Lab

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
+function calculateTax(amount) {
+  return amount * 0.10;
+}
 
+function convertToUpperCase(text) {
+  return text.toUpperCase();
+}
+
+function findMaximum(num1, num2) {
+  return num1 > num2 ? num1 : num2;
+}
+
+function isPalindrome(word) {
+  let normalized = word.toLowerCase();
+  let reversed = normalized.split('').reverse().join('');
+  return normalized === reversed;
+}
+
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  return originalPrice * (1 - discountPercentage / 100);
+}
 
 
 
